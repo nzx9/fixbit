d050c2be439b56e1a7ffa008b824443ec66fa6d2
navindu <navindudananga123@gmail.com>
2020-11-14 16:15:36 +0530

    created teams and member classes

---
+++ api/teams/team.php
@@ -0,0 +1,34 @@
+<?php
+class Team
+{
+    private $connection;
+    private $table = "teams";
+
+    private $tid;
+    private $name;
+
+    public function __construct($db)
+    {
+        $this->connection = $db;
+    }
+
+    public function create()
+    {
+        if (!empty($this->name)) {
+            $query = "INSERT INTO " . $this->table . "(name) VALUES (:name)";
+
+            $stmt = $this->connection->prepare($query);
+
+            $this->name = htmlspecialchars(strip_tags($this->name));
+
+            $stmt->bindParam(":name", $this->name);
+
+            if ($stmt->execute()) {
+                $row = $stmt->fetch(PDO::FETCH_ASSOC);
+                $this->tid = $row['tid'];
+                return true;
+            }
+        }
+        return false;
+    }
+}
