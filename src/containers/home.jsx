625595cb857149929d3b236a07a27875e250d32d
navindu <navindudananga123@gmail.com>
2020-11-15 20:54:55 +0530

    improved ui

--- src/containers/home.jsx
+++ src/containers/home.jsx
@@ -1,30 +1,87 @@
 import React from "react";
 import {
   getUId,
-  getFirstName,
-  getLastName,
+  getUserName,
+  getFullName,
   getEmail,
 } from "../reducers/userDataTracker";
-import { useSelector } from "react-redux";
+import { Button, Container, Paper, makeStyles, Grid } from "@material-ui/core";
+import { useSelector, useDispatch } from "react-redux";
+import { logout } from "../reducers/loginTracker";
+import { useSnackbar } from "notistack";
+
+const settings = require("../components/settings.json");
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  paper2: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.grey[100],
+    backgroundColor: theme.palette.grey[900],
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+
+  button: {
+    backgroundColor: theme.palette.warning.main,
+    color: theme.palette.text.secondary,
+    "&:hover": {
+      backgroundColor: theme.palette.warning.dark,
+    },
+  },
+}));
 
 const Home = () => {
   const uid = useSelector(getUId);
-  const firstname = useSelector(getFirstName);
-  const lastname = useSelector(getLastName);
+  const username = useSelector(getUserName);
+  const fullname = useSelector(getFullName);
   const email = useSelector(getEmail);
+  const classes = useStyles();
+  const dispath = useDispatch();
+  const { enqueueSnackbar } = useSnackbar();
   return (
     <div>
-      <h1>Welcome {firstname}!</h1>
-      <code>
-        <span>{"-->"}</span>Info
-      </code>
-      <ul>
-        <li>User Id : {uid}</li>
-        <li>First Name: {firstname}</li>
-        <li>Last Name : {lastname}</li>
-        <li>Email : {email}</li>
-        <li>Build type: {process.env.NODE_ENV}</li>
-      </ul>
+      <Container component="main" maxWidth="xl">
+        <Paper className={classes.paper}>
+          <h1>Welcome {username}!</h1>
+          <Button
+            className={classes.button}
+            onClick={() => {
+              enqueueSnackbar("Logout", {
+                variant: "warning",
+                anchorOrigin: settings.snackbar.anchorOrigin,
+              });
+              dispath(logout());
+            }}
+            variant="contained"
+          >
+            logout
+          </Button>
+          <Paper className={classes.paper2}>
+            <Grid container>User Id: {uid}</Grid>
+            <Grid container>User Name: {username}</Grid>
+            <Grid container>Full Name: {fullname}</Grid>
+            <Grid container>Email : {email}</Grid>
+          </Paper>
+          <Grid container justify="flex-end">
+            <code>{process.env.NODE_ENV} build</code>
+          </Grid>
+        </Paper>
+      </Container>
     </div>
   );
 };
