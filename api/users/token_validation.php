a8c9f180f4b499988cf0f28d6ee82ac8a962c681
navindu <navindudananga123@gmail.com>
2020-11-17 10:29:43 +0530

    token validating support added

--- api/users/token_validation.php
+++ api/users/token_validation.php
@@ -3,28 +3,25 @@ require '../vendor/autoload.php';
 
 use \Firebase\JWT\JWT;
 
-// $key = "example_key";
-
-
-
 class Token
 {
     private $key = "hello_world";
     private $v_algo = 'HS256';
+    private $site_url = "http://localhost";
 
     public function __construct($time = 60)
     {
         JWT::$leeway = $time; // leeway time in seconds (default = 60s)
     }
 
-    public function generate($payload)
+    public function generate($uid)
     {
-        // $payload = array(
-        //     "iss" => "http://example.org",
-        //     "aud" => "http://example.com",
-        //     "iat" => 1356999524,
-        //     "nbf" => 1357000000,
-        // );
+        $payload = array(
+            "iss" => $this->site_url,
+            "uid" => $uid,
+            "iat" => time() + (60 * 60),
+            "nbf" => time(),
+        );
         $jwt = JWT::encode($payload, $this->key);
         return $jwt;
     }
