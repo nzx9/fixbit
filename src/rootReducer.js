d9e2f19a4130ea6af2f830d4bc0321670a86ae22
navindu <navindudananga123@gmail.com>
2020-11-18 15:03:51 +0530

    added token tracker

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -3,11 +3,13 @@ import { connectRouter } from "connected-react-router";
 
 import loginReducer from "./reducers/loginTracker";
 import userDataReducer from "./reducers/userDataTracker";
+import tokenReducer from "./reducers/tokenTracker";
 
 export default function createRootReducer(history) {
   return combineReducers({
     router: connectRouter(history),
     isLogged: loginReducer,
     userData: userDataReducer,
+    token: tokenReducer,
   });
 }
