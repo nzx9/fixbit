cddaa0eaed2ba5e2993abc15707169f458221bb4
navindu <navindudananga123@gmail.com>
2020-11-23 13:19:52 +0530

    created projects and teams containers

---
+++ src/containers/projects.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Projects = () => {
+  return 0;
+};
+
+export default Projects;
---
+++ src/containers/teams.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Teams = () => {
+  return 0;
+};
+
+export default Teams;
