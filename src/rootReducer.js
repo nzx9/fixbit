b36ab47c1ec9965a85327892ddfbc213d8488929
navindu <navindudananga123@gmail.com>
2020-11-26 00:05:00 +0530

    added isDataChanged reducer

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -4,6 +4,7 @@ import { connectRouter } from "connected-react-router";
 import loginReducer from "./reducers/loginTracker";
 import userDataReducer from "./reducers/userDataTracker";
 import tokenReducer from "./reducers/tokenTracker";
+import dataChangeReducer from "./reducers/dataChangeTracker";
 
 export default function createRootReducer(history) {
   return combineReducers({
@@ -11,5 +12,6 @@ export default function createRootReducer(history) {
     isLogged: loginReducer,
     userData: userDataReducer,
     token: tokenReducer,
+    isDataChanged: dataChangeReducer,
   });
 }
