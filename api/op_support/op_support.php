bc5cc503ca0734374fcaee7ebf049b04a8fcab56
navindu <navindudananga123@gmail.com>
2020-12-06 19:15:45 +0530

    op_support added to get support for search queries

---
+++ api/op_support/op_support.php
@@ -0,0 +1,50 @@
+<?php
+class OpSupport
+{
+    private $connection;
+    private $PU_SEARCH = "project_users_search_table";
+
+    public function __construct($db)
+    {
+        $this->connection = $db;
+    }
+
+    public function addToProjectUserSearchSupport($uid, $pid, $isAdmin, $isPublic)
+    {
+        if (!empty($uid) && !empty($pid)) {
+            $query = "INSERT INTO " . $this->PU_SEARCH . " (uid, pid, isAdmin, isPublic) 
+                                               VALUES (:uid, :pid, :isAdmin, :isPublic)";
+
+            $stmt = $this->connection->prepare($query);
+
+            $uid = htmlspecialchars(strip_tags($uid));
+            $pid = htmlspecialchars(strip_tags($pid));
+            $isAdmin = htmlspecialchars(strip_tags($isAdmin));
+            $isPublic = htmlspecialchars(strip_tags($isPublic));
+
+
+            $stmt->bindParam(":uid", $uid);
+            $stmt->bindParam(":pid", $pid);
+            $stmt->bindParam(":isAdmin", $isAdmin);
+            $stmt->bindParam(":isPublic", $isPublic);
+
+            if ($stmt->execute() && $stmt->rowCount() > 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function getProjectsByUser()
+    {
+        $query = "SELECT * FROM " .  $this->PU_SEARCH . " WHERE isPublic = 1 OR uid = :uid";
+        $stmt = $this->connection->prepare($query);
+        $stmt->execute();
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $data[] = array(
+                "pid" => $row['pid']
+            );
+        }
+        return $data;
+    }
+}
