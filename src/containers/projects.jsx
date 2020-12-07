ef4c4bf71b5165521346f42cf07796086e6afdbe
navindu <navindudananga123@gmail.com>
2020-12-07 11:13:48 +0530

    cpu, memory leak and loop of api calling fixed

--- src/containers/projects.jsx
+++ src/containers/projects.jsx
@@ -12,12 +12,39 @@ import ProjectCard from "./project-card";
 
 import router from "../routes/routes.json";
 
-import { Backdrop, CircularProgress, makeStyles } from "@material-ui/core";
+import { Backdrop, CircularProgress, Fab, makeStyles } from "@material-ui/core";
+import { Add } from "@material-ui/icons";
 
 const useStyles = makeStyles((theme) => ({
   backdrop: {
     zIndex: theme.zIndex.drawer + 1,
     color: theme.palette.primary.main,
+  },
+  fabDesktop: {
+    display: "flex",
+    position: "fixed",
+    zIndex: 9999,
+    bottom: theme.spacing(3),
+    right: theme.spacing(2),
+    [theme.breakpoints.down("sm")]: {
+      display: "none",
+    },
+  },
+  fabMobile: {
+    display: "none",
+    position: "fixed",
+    zIndex: 9999,
+    bottom: theme.spacing(2),
+    right: theme.spacing(2),
+    [theme.breakpoints.down("sm")]: {
+      display: "flex",
+    },
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1),
+  },
+  fabText: {
+    display: "flex",
   },
 }));
 
@@ -29,7 +56,8 @@ const Projects = () => {
   const [isLoaded, setIsLoaded] = React.useState(false);
   const [error, setError] = React.useState(null);
   const [data, setData] = React.useState([]);
-  const [_openBackdrop, _setOpenBackdrop] = React.useState(false);
+  const [filterValue, setFilterValue] = React.useState("ALL");
+  const [newProjectAdded, setNewProjectAdded] = React.useState(false);
 
   const history = useHistory();
   const goto = useCallback((path) => history.push(path), [history]);
@@ -62,13 +90,9 @@ const Projects = () => {
       });
   };
 
-  // const setOpenBackdrop = (value) => {
-  //   _setOpenBackdrop(value);
-  // };
-
   useEffect(() => {
-    fetchDataAndSet("ALL");
-  });
+    fetchDataAndSet(filterValue);
+  }, [filterValue, newProjectAdded]);
 
   if (error) {
     return (
@@ -85,15 +109,24 @@ const Projects = () => {
       </div>
     );
   return (
-    <div
-      style={{
-        display: "auto",
-        marginLeft: "3%",
-      }}
-    >
-      {data.map((value, index) => (
-        <ProjectCard key={index} data={value} />
-      ))}
+    <div>
+      <Fab variant="extended" className={classes.fabDesktop} color="primary">
+        <Add className={classes.extendedIcon} />
+        <div className={classes.fabText}>New Project</div>
+      </Fab>
+      <Fab className={classes.fabMobile} color="primary">
+        <Add />
+      </Fab>
+      <div
+        style={{
+          display: "auto",
+          marginLeft: "3%",
+        }}
+      >
+        {data.map((value, index) => (
+          <ProjectCard key={index} data={value} />
+        ))}
+      </div>
     </div>
   );
 };
