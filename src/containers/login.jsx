a54e1d17cdf959fb6e170c83a225a68cc6742c7a
navindu <navindudananga123@gmail.com>
2020-12-22 11:19:10 +0530

    changes to access new API and fixes

--- src/containers/login.jsx
+++ src/containers/login.jsx
@@ -13,7 +13,7 @@ import {
 import { useSnackbar } from "notistack";
 import { Link, useHistory } from "react-router-dom";
 import routes from "../routes/routes.json";
-import { httpPOST } from "../components/httpRequest";
+import { httpReq } from "../components/httpRequest";
 import { useDispatch } from "react-redux";
 import { login } from "../reducers/loginTracker";
 import {
@@ -24,8 +24,9 @@ import {
 } from "../reducers/userDataTracker";
 import { setToken } from "../reducers/tokenTracker";
 import { DEBUG_PRINT } from "../components/debugTools";
-
-const settings = require("../components/settings.json");
+import { NOTIFY, AlertDialog } from "../components/notify";
+import settings from "../components/settings.json";
+import config from "../components/config.json";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -57,13 +58,16 @@ const Login = () => {
   const [_email, _setEmail] = React.useState(null);
   const [_password, _setPassword] = React.useState(null);
   const [_openBackdrop, _setOpenBackdrop] = React.useState(false);
+  const [alertOpen, setAlertOpen] = React.useState(false);
+  const [alertType, setAlertType] = React.useState(null);
+  const [alertTitle, setAlertTitle] = React.useState(null);
+  const [alertMsg, setAlertMsg] = React.useState(null);
 
-  const emailInputHandler = (e) => {
-    _setEmail(e.target.value);
-  };
-  const passwordInputHandler = (e) => {
-    _setPassword(e.target.value);
-  };
+  const emailInputHandler = (e) => _setEmail(e.target.value);
+
+  const passwordInputHandler = (e) => _setPassword(e.target.value);
+
+  const handleAlertClose = () => setAlertOpen(false);
 
   const history = useHistory();
   const goToHome = useCallback(() => history.push(routes.HOME), [history]);
@@ -75,39 +79,48 @@ const Login = () => {
       autoComplete="on"
       onSubmit={(e) => {
         _setOpenBackdrop(true);
-        httpPOST(
-          `${window.location.protocol}//${window.location.hostname}/api/users/login.php`,
-          {
-            email: _email,
-            password: _password,
-          }
-        )
+        httpReq(`${config.URL}/api/users/login`, "POST", {
+          email: _email,
+          password: _password,
+        })
           .then((res) => {
             DEBUG_PRINT(res);
-            _setOpenBackdrop(false);
-            if (res.success) {
-              enqueueSnackbar("Success", {
-                variant: "success",
-                anchorOrigin: settings.snackbar.anchorOrigin,
+            res.json().then((r) => {
+              NOTIFY(r.msg, (msg) => {
+                _setOpenBackdrop(false);
+                enqueueSnackbar(msg, {
+                  variant: r.type,
+                  anchorOrigin: settings.snackbar.anchorOrigin,
+                });
+                if (res.status === 200 && r.success === true) {
+                  dispatch(setUId(r.data.id));
+                  dispatch(setUserName(r.data.username));
+                  dispatch(setFullName(r.data.fullname));
+                  dispatch(setEmail(r.data.email));
+                  dispatch(setToken(r.token));
+                  localStorage.setItem("token", r.token);
+                  goToHome();
+                  dispatch(login());
+                }
               });
-              dispatch(setUId(res.user_data.uid));
-              dispatch(setUserName(res.user_data.username));
-              dispatch(setFullName(res.user_data.fullname));
-              dispatch(setEmail(res.user_data.email));
-              dispatch(setToken(res.user_data.token));
-              goToHome();
-              dispatch(login());
-            } else {
-              _setOpenBackdrop(false);
-              enqueueSnackbar(res.msg, {
-                variant: "error",
-                anchorOrigin: settings.snackbar.anchorOrigin,
-              });
-            }
+            });
           })
           .catch((err) => {
+            DEBUG_PRINT(err);
             _setOpenBackdrop(false);
-            alert(err);
+            setAlertType("error");
+            const parsedError = err.toString().split(":");
+            if (parsedError.length >= 1) {
+              setAlertTitle(parsedError[0].trim());
+            } else {
+              setAlertTitle("Error");
+            }
+            if (parsedError.length >= 2) {
+              setAlertMsg(parsedError[1].trim());
+            } else {
+              setAlertMsg("Unknown");
+            }
+            setAlertOpen(true);
           });
         e.preventDefault();
       }}
@@ -117,6 +130,13 @@ const Login = () => {
         <Backdrop className={classes.backdrop} open={_openBackdrop}>
           <CircularProgress color="inherit" />
         </Backdrop>
+        <AlertDialog
+          alertOpen={alertOpen}
+          title={alertTitle}
+          type={alertType}
+          msg={alertMsg}
+          handleAlertClose={() => handleAlertClose()}
+        />
         <Paper className={classes.paper}>
           <h1>Login</h1>
           <TextField
