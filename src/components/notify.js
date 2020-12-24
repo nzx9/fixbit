2448661e220efbcca4a6af7622597c6ffe4840dd
navindu <navindudananga123@gmail.com>
2020-12-24 23:52:51 +0530

    new function added

--- src/components/notify.js
+++ src/components/notify.js
@@ -8,6 +8,7 @@ import {
   DialogTitle,
   Grid,
   makeStyles,
+  TextField,
 } from "@material-ui/core";
 import { Cancel, CheckCircle, Help } from "@material-ui/icons";
 
@@ -65,6 +66,69 @@ export function AlertDialog(props) {
             }
           >
             Ok
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
+export function AlertDialogConfirmation(props) {
+  const classes = useStyles();
+  return (
+    <div>
+      <Dialog
+        open={props.alertOpen}
+        onClose={props.handleAlertClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          <Grid container>
+            <Grid item style={{ marginRight: 5 }}>
+              {props.type === "error" ? (
+                <Cancel className={classes.error} fontSize="large" />
+              ) : props.type === "success" ? (
+                <CheckCircle className={classes.success} fontSize="large" />
+              ) : (
+                <Help className={classes.warning} fontSize="large" />
+              )}
+            </Grid>
+            <Grid item>{props.title}</Grid>
+          </Grid>
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            {props.msg}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={() => {
+              props.rejectCallback();
+            }}
+            className={
+              props.type === "success"
+                ? classes.success
+                : props.type === "error"
+                ? classes.error
+                : classes.warning
+            }
+          >
+            No
+          </Button>
+          <Button
+            onClick={() => {
+              props.resolveCallback();
+            }}
+            className={
+              props.type === "success"
+                ? classes.success
+                : props.type === "error"
+                ? classes.error
+                : classes.warning
+            }
+          >
+            Yes
           </Button>
         </DialogActions>
       </Dialog>
