3edd1a14732b9a9ec4eb16bcdb2c8593c9073b67
navindu <navindudananga123@gmail.com>
2021-01-07 19:06:39 +0530

    team view UI completed

--- src/components/notify.js
+++ src/components/notify.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   Button,
   Dialog,
@@ -9,8 +9,11 @@ import {
   Grid,
   makeStyles,
   TextField,
+  Box,
 } from "@material-ui/core";
 import { Cancel, CheckCircle, Help } from "@material-ui/icons";
+import { httpReq } from "./httpRequest";
+import config from "../components/config.json";
 
 const useStyles = makeStyles((theme) => ({
   success: {
@@ -129,6 +132,125 @@ export function AlertDialogConfirmation(props) {
             }
           >
             Yes
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
+export function NewMemberAddAlert({
+  alertOpen,
+  handleAlertClose,
+  type,
+  title,
+  msg,
+  resolveCallback,
+  rejectCallback,
+  token,
+}) {
+  const classes = useStyles();
+  const [userId, setUserId] = React.useState(0);
+  const [userData, setUserData] = React.useState(null);
+  const [error, setError] = React.useState(null);
+  const [isLoaded, setIsLoaded] = React.useState(false);
+
+  const handleUserIdChange = (e) => {
+    setUserId(parseInt(e.target.value));
+  };
+
+  useEffect(() => {
+    setError(null);
+    setIsLoaded(false);
+    if (userId > 0) {
+      httpReq(`${config.URL}/api/users/user/${userId}`, "GET", null, token)
+        .then((res) => {
+          res.json().then((r) => {
+            setIsLoaded(true);
+            if (res.status === 200 && r.success === true) {
+              setUserData(r.data);
+            } else {
+              setError(r.msg);
+              setUserData(null);
+            }
+          });
+        })
+        .catch((err) => {
+          setIsLoaded(true);
+          setError(err.message);
+          setUserData(null);
+        });
+    } else {
+      setError("Invalid user Id");
+    }
+  }, [userId]);
+  return (
+    <div>
+      <Dialog
+        open={alertOpen}
+        onClose={handleAlertClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          <Grid container>
+            <Grid item style={{ marginRight: 5 }}></Grid>
+            <Grid item>
+              <b>{title.toUpperCase()}</b>
+            </Grid>
+          </Grid>
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            {msg}
+          </DialogContentText>
+          <TextField
+            type="number"
+            value={userId}
+            variant="outlined"
+            fullWidth
+            onChange={handleUserIdChange}
+          />
+          <Box>
+            <pre>
+              {error ? (
+                <p>
+                  <b>error : </b>
+                  <span className={classes.error}>{error}</span>
+                </p>
+              ) : !isLoaded ? (
+                <p>
+                  <b>Please wait...</b>
+                </p>
+              ) : (
+                <div>
+                  user id&nbsp;&nbsp;: {userData?.id}
+                  <br />
+                  username&nbsp;: {userData?.username}
+                  <br />
+                  fullname&nbsp;: {userData?.fullname}
+                  <br />
+                  email&nbsp;&nbsp;&nbsp;&nbsp;: {userData?.email}
+                </div>
+              )}
+            </pre>
+          </Box>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={() => {
+              rejectCallback();
+            }}
+            className={classes.error}
+          >
+            Cancel
+          </Button>
+          <Button
+            onClick={() => {
+              resolveCallback(userData);
+            }}
+            className={classes.success}
+          >
+            Add Member
           </Button>
         </DialogActions>
       </Dialog>
--- src/containers/view-team.jsx
+++ src/containers/view-team.jsx
@@ -1,7 +1,447 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { IconButton, Link } from "@material-ui/core";
+import { httpReq } from "../components/httpRequest";
+import { useSnackbar } from "notistack";
+import { useSelector } from "react-redux";
+import { getUId } from "../reducers/userDataTracker";
+import { getToken } from "../reducers/tokenTracker";
+import {
+  Grid,
+  Box,
+  CardActionArea,
+  Card,
+  CardContent,
+  CardMedia,
+  Backdrop,
+  CircularProgress,
+  makeStyles,
+} from "@material-ui/core/";
+import { useFourThreeCardMediaStyles } from "@mui-treasury/styles/cardMedia/fourThree";
+import { Info, InfoSubtitle, InfoTitle } from "@mui-treasury/components/info";
+import { useApexInfoStyles } from "@mui-treasury/styles/info/apex";
+import {
+  GitHub,
+  Twitter,
+  LinkedIn,
+  AddCircle,
+  Cancel,
+} from "@material-ui/icons";
+import { NOTIFY, AlertDialog, NewMemberAddAlert } from "../components/notify";
+import { DEBUG_PRINT } from "../components/debugTools";
+import { randomColor } from "../components/random-color-generator";
+import config from "../components/config.json";
+import settings from "../components/settings.json";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    float: "left",
+    display: "block",
+    width: 175,
+    height: 250,
+    margin: 10,
+    maxWidth: 200,
+    maxHeight: 250,
+  },
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 200,
+    color: theme.palette.primary.main,
+  },
+  actionArea: {
+    borderRadius: 16,
+    transition: "0.2s",
+    "&:hover": {
+      transform: "scale(1.1)",
+    },
+  },
+  card: ({ color }) => ({
+    borderRadius: 16,
+    height: 250,
+    boxShadow: "none",
+    "&:hover": {
+      boxShadow: `0 6px 12px 0 ${color}
+        .rotate(-12)
+        .darken(0.2)
+        .fade(0.5)}`,
+    },
+  }),
+  content: ({ color }) => {
+    return {
+      backgroundColor: color,
+      padding: "1rem 1.5rem 1.5rem",
+    };
+  },
+  addNewContent: ({ color }) => {
+    return {
+      height: 250,
+      backgroundColor: color,
+      padding: "1rem 1.5rem 1.5rem",
+    };
+  },
+  title: {},
+  subtitle: {},
+  addNewBtn: {
+    "&:hover": {
+      transform: "scale(1.2)",
+    },
+  },
+  closeBtn: {
+    color: theme.palette.error.main,
+  },
+  twitter: {
+    color: "#1DA1F2",
+    "&:hover": {
+      transform: "scale(1.5)",
+      color: "#ffffff",
+    },
+  },
+  linkedIn: {
+    color: "#0077B5",
+    "&:hover": {
+      transform: "scale(1.5)",
+      color: "#ffffff",
+    },
+  },
+  github: {
+    color: "#181717",
+    "&:hover": {
+      transform: "scale(1.5)",
+      color: "#ffffff",
+    },
+  },
+}));
+
+const UserCard = ({
+  classes,
+  image,
+  username,
+  fullname,
+  tid,
+  uid,
+  showDelete = false,
+  openBackdrop,
+  closeBackdrop,
+  action,
+}) => {
+  const mediaStyles = useFourThreeCardMediaStyles();
+  const { enqueueSnackbar } = useSnackbar();
+  const token = useSelector(getToken);
+  return (
+    <div className={classes.root}>
+      <CardActionArea className={classes.actionArea}>
+        <Info useStyles={useApexInfoStyles}>
+          <Card className={classes.card}>
+            <CardMedia classes={mediaStyles} image={image}>
+              {showDelete ? (
+                <div style={{ position: "absolute", top: 2, right: 2 }}>
+                  <IconButton
+                    className={classes.closeBtn}
+                    onClick={() => {
+                      openBackdrop();
+                      httpReq(
+                        `${config.URL}/api/teams/${tid}/members/${uid}`,
+                        "DELETE",
+                        null,
+                        token
+                      )
+                        .then((res) => {
+                          res.json().then((r) => {
+                            NOTIFY(r.msg, (msg) => {
+                              closeBackdrop();
+                              if (msg === null || msg === undefined)
+                                msg = r.message;
+                              enqueueSnackbar(msg, {
+                                variant: r.type,
+                                anchorOrigin: settings.snackbar.anchorOrigin,
+                              });
+                              DEBUG_PRINT(r);
+                              if (res.status === 200 && r.success === true)
+                                action();
+                            });
+                          });
+                        })
+                        .catch((err) => {
+                          closeBackdrop();
+                          enqueueSnackbar(err?.message, {
+                            variant: "error",
+                            anchorOrigin: settings.snackbar.anchorOrigin,
+                          });
+                        });
+                    }}
+                  >
+                    <Cancel fontSize="small" />
+                  </IconButton>
+                </div>
+              ) : null}
+            </CardMedia>
+            <CardContent className={classes.content}>
+              <InfoTitle className={classes.title}>{username}</InfoTitle>
+              <InfoSubtitle className={classes.subtitle}>
+                {fullname}
+              </InfoSubtitle>
+              <Grid container justify="space-evenly" style={{ marginTop: 30 }}>
+                <Grid item>
+                  <Link href="https://twitter.com/">
+                    <Twitter className={classes.twitter} />
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="https://linkedin.com/">
+                    <LinkedIn className={classes.linkedIn} />
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="https://github.com/">
+                    <GitHub className={classes.github} />
+                  </Link>
+                </Grid>
+              </Grid>
+            </CardContent>
+          </Card>
+        </Info>
+      </CardActionArea>
+    </div>
+  );
+};
+
+const AddNewCard = ({ classes, handleOpenAddMemberAlert }) => {
+  return (
+    <div className={classes.root}>
+      <CardActionArea className={classes.actionArea}>
+        <Card className={classes.card}>
+          <CardContent className={classes.addNewContent}>
+            <Box
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              style={{
+                position: "absolute",
+                left: "50%",
+                top: "50%",
+                transform: "translate(-50%, -50%)",
+              }}
+            >
+              <IconButton
+                className={classes.addNewBtn}
+                title="Add New Member"
+                size="medium"
+                onClick={handleOpenAddMemberAlert}
+              >
+                <AddCircle fontSize="large" style={{ color: randomColor() }} />
+              </IconButton>
+            </Box>
+          </CardContent>
+        </Card>
+      </CardActionArea>
+    </div>
+  );
+};
 
 const ViewTeam = (props) => {
-  return <p>Team: {props.match.params.tid}</p>;
+  const classes = useStyles({ color: randomColor() });
+  const token = useSelector(getToken);
+  const uId = useSelector(getUId);
+  const [error, setError] = React.useState(false);
+  const [isMDLoaded, setIsMDLoaded] = React.useState(false);
+  const [isTDLoaded, setIsTDLoaded] = React.useState(false);
+  const [mData, setMData] = React.useState([]);
+  const [tData, setTData] = React.useState([]);
+  const [alertType, setAlertType] = React.useState();
+  const [alertTitle, setAlertTitle] = React.useState();
+  const [alertMsg, setAlertMsg] = React.useState();
+  const [alertOpen, setAlertOpen] = React.useState(false);
+  const [addMemberAlert, setAddMemberAlert] = React.useState(false);
+
+  const { enqueueSnackbar } = useSnackbar();
+  const [_openBackdrop, _setOpenBackdrop] = React.useState(false);
+
+  const fetchMemberDataAndSet = () => {
+    _setOpenBackdrop(true);
+    httpReq(
+      `${config.URL}/api/teams/${props.match.params.tid}/members`,
+      "GET",
+      null,
+      token
+    )
+      .then((res) => {
+        setIsMDLoaded(true);
+        DEBUG_PRINT(res);
+        res.json().then((r) => {
+          NOTIFY(r.msg, (msg) => {
+            _setOpenBackdrop(false);
+            if (msg === null || msg === undefined) msg = r.message;
+            enqueueSnackbar(msg, {
+              variant: r.type,
+              anchorOrigin: settings.snackbar.anchorOrigin,
+            });
+            DEBUG_PRINT(r);
+            if (res.status === 200 && r.success === true)
+              r.data !== null ? setMData(r.data) : setMData([]);
+            else setError(r.msg);
+          });
+        });
+      })
+      .catch((err) => {
+        _setOpenBackdrop(false);
+        setIsMDLoaded(true);
+        setAlertType("error");
+        const parsedError = err.toString().split(":");
+        if (parsedError.length >= 1) {
+          setAlertTitle(parsedError[0].trim());
+        } else {
+          setAlertTitle("Error");
+        }
+        if (parsedError.length >= 2) {
+          setAlertMsg(parsedError[1].trim());
+        } else {
+          setAlertMsg("Unknown");
+        }
+        setAlertOpen(true);
+        setError(err.message);
+      });
+  };
+
+  const fetchTeamDataAndSet = () => {
+    _setOpenBackdrop(true);
+    httpReq(
+      `${config.URL}/api/teams/${props.match.params.tid}`,
+      "GET",
+      null,
+      token
+    )
+      .then((res) => {
+        setIsTDLoaded(true);
+        DEBUG_PRINT(res);
+        res.json().then((r) => {
+          NOTIFY(r.msg, (msg) => {
+            _setOpenBackdrop(false);
+            if (msg === null || msg === undefined) msg = r.message;
+            enqueueSnackbar(msg, {
+              variant: r.type === "success" ? "info" : "error",
+              anchorOrigin: settings.snackbar.anchorOrigin,
+            });
+            DEBUG_PRINT(r);
+            if (res.status === 200 && r.success === true)
+              r.data !== null ? setTData(r.data) : setTData({});
+            else setError(r.msg);
+          });
+        });
+      })
+      .catch((err) => {
+        setIsTDLoaded(true);
+        _setOpenBackdrop(false);
+        setAlertType("error");
+        const parsedError = err.toString().split(":");
+        if (parsedError.length >= 1) {
+          setAlertTitle(parsedError[0].trim());
+        } else {
+          setAlertTitle("Error");
+        }
+        if (parsedError.length >= 2) {
+          setAlertMsg(parsedError[1].trim());
+        } else {
+          setAlertMsg("Unknown");
+        }
+        setAlertOpen(true);
+        setError(err.message);
+      });
+  };
+
+  useEffect(() => {
+    fetchMemberDataAndSet();
+    fetchTeamDataAndSet();
+  }, []);
+
+  if (error) return <p>Error: {error}</p>;
+  else if (!isMDLoaded || !isTDLoaded)
+    return (
+      <Backdrop className={classes.backdrop} open="true">
+        <CircularProgress color="inherit" />
+      </Backdrop>
+    );
+  else
+    return (
+      <div>
+        <Backdrop className={classes.backdrop} open={_openBackdrop}>
+          <CircularProgress color="inherit" />
+        </Backdrop>
+        <AlertDialog
+          alertOpen={alertOpen}
+          title={alertTitle}
+          type={alertType}
+          msg={alertMsg}
+          handleAlertClose={() => setAlertOpen(false)}
+        />
+        <div style={{ marginLeft: "3%" }}>
+          {mData.map((value, index) => (
+            <UserCard
+              key={index}
+              classes={classes}
+              username={value.info.username}
+              fullname={value.info.fullname}
+              uid={value.info.id}
+              tid={props.match.params.tid}
+              openBackdrop={() => _setOpenBackdrop(true)}
+              closeBackdrop={() => _setOpenBackdrop(false)}
+              action={() => fetchMemberDataAndSet()}
+              showDelete={
+                uId === tData?.leader_id && value.info.id !== uId ? true : false
+              }
+              image={`https://ui-avatars.com/api/?name=${value.info.fullname}&size=512&background=random`}
+            />
+          ))}
+          {uId === tData.leader_id ? (
+            <AddNewCard
+              key={mData.length + 1}
+              classes={classes}
+              handleOpenAddMemberAlert={() => setAddMemberAlert(true)}
+            />
+          ) : null}
+        </div>
+        <NewMemberAddAlert
+          alertOpen={addMemberAlert}
+          type="warning"
+          title="Add Member to Team"
+          msg="Enter User ID of the member to add:"
+          token={token}
+          rejectCallback={() => setAddMemberAlert(false)}
+          resolveCallback={(data) => {
+            _setOpenBackdrop(true);
+            if (data && data.id !== null && data.id !== undefined) {
+              httpReq(
+                `${config.URL}/api/teams/${props.match.params.tid}/members`,
+                "POST",
+                { uid: data.id },
+                token
+              )
+                .then((res) => {
+                  res.json().then((r) => {
+                    NOTIFY(r.msg, (msg) => {
+                      _setOpenBackdrop(false);
+                      if (msg === null || msg === undefined) msg = r.message;
+                      enqueueSnackbar(msg, {
+                        variant: r.type,
+                        anchorOrigin: settings.snackbar.anchorOrigin,
+                      });
+                      DEBUG_PRINT(r);
+                      if (res.status === 201 && r.success === true)
+                        fetchMemberDataAndSet();
+                      setAddMemberAlert(false);
+                    });
+                  });
+                })
+                .catch((err) => {
+                  _setOpenBackdrop(false);
+                  enqueueSnackbar(err?.message, {
+                    variant: "error",
+                    anchorOrigin: settings.snackbar.anchorOrigin,
+                  });
+                });
+              setAddMemberAlert(false);
+            }
+          }}
+        />
+      </div>
+    );
 };
 
 export default ViewTeam;
