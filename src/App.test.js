a5cd719ec5851bf12304d21fb9619e81ddf70be2
navindu <navindudananga123@gmail.com>
2021-01-09 23:28:44 +0530

    Test updated -4

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders Login", () => {
+test("renders Login check for Register Link", () => {
   render(<App />);
-  const linkElement = screen.getByText("<h1>Login</h1>");
+  const linkElement = screen.getByText(/Register/i);
   expect(linkElement).toBeInTheDocument();
 });
