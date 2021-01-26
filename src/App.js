8d5e1a144e936b05b4f12f89ec22285330b017a4
navindu <navindudananga123@gmail.com>
2021-01-26 12:56:27 +0530

    improved:: MaxSnackbars can customize improved:: dark mode/ light mode customize

--- src/App.js
+++ src/App.js
@@ -4,17 +4,35 @@ import { ConnectedRouter } from "connected-react-router";
 import Router from "./routes/Router";
 import { SnackbarProvider } from "notistack";
 import { configuredStore, history } from "./store";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core";
 
 const App = () => {
   const store = configuredStore();
+  const selected_theme = localStorage.getItem("theme");
+  const maxSnacks =
+    localStorage.getItem("max_snacks") <= 5
+      ? localStorage.getItem("max_snacks")
+      : 3;
+
+  const theme = createMuiTheme({
+    palette: {
+      type: selected_theme === "dark" ? "dark" : "light",
+      background: {
+        paper: selected_theme === "dark" ? "#31373d" : "#fff",
+      },
+    },
+  });
+
   return (
-    <SnackbarProvider maxSnack={3}>
-      <Provider store={store}>
-        <ConnectedRouter history={history}>
-          <Router />
-        </ConnectedRouter>
-      </Provider>
-    </SnackbarProvider>
+    <ThemeProvider theme={theme}>
+      <SnackbarProvider maxSnack={maxSnacks}>
+        <Provider store={store}>
+          <ConnectedRouter history={history}>
+            <Router />
+          </ConnectedRouter>
+        </Provider>
+      </SnackbarProvider>
+    </ThemeProvider>
   );
 };
 
