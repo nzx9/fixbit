7dc1e62a15ad8de72b5701040f7aebfc878e13f4
navindu <navindudananga123@gmail.com>
2021-01-28 20:33:15 +0530

    remarkable configured. Syntax highlighting supported

--- src/containers/view-issue.jsx
+++ src/containers/view-issue.jsx
@@ -24,8 +24,8 @@ import { Info, InfoTitle, InfoSubtitle } from "@mui-treasury/components/info";
 import { FiberManualRecord } from "@material-ui/icons";
 import ChatMsg from "@mui-treasury/components/chatMsg/ChatMsg";
 import { useApexInfoStyles } from "@mui-treasury/styles/info/apex";
-import hljs from "highlight.js";
 import { Remarkable } from "remarkable";
+import hljs from "highlight.js";
 
 const useStyles = makeStyles((theme) => ({
   backdrop: {
@@ -69,17 +69,33 @@ const ViewIssue = (props) => {
   const [error, setError] = React.useState(null);
   const [isLoaded, setIsLoaded] = React.useState(false);
 
-  var md = new Remarkable({
+  var md = new Remarkable("full", {
+    html: false, // Enable HTML tags in source
+    xhtmlOut: false, // Use '/' to close single tags (<br />)
+    breaks: false, // Convert '\n' in paragraphs into <br>
+    langPrefix: "language-", // CSS language prefix for fenced blocks
+    linkify: true, // autoconvert URL-like texts to links
+    linkTarget: "", // set target to open link in
+
+    // Enable some language-neutral replacements + quotes beautification
+    typographer: false,
+
+    // Double + single quotes replacement pairs, when typographer enabled,
+    // and smartquotes on. Set doubles to '«»' for Russian, '„“' for German.
+    quotes: "“”‘’",
+
+    // Highlighter function. Should return escaped HTML,
+    // or '' if input not changed
     highlight: function (str, lang) {
       if (lang && hljs.getLanguage(lang)) {
         try {
           return hljs.highlight(lang, str).value;
-        } catch (err) {}
+        } catch (__) {}
       }
 
       try {
         return hljs.highlightAuto(str).value;
-      } catch (err) {}
+      } catch (__) {}
 
       return ""; // use external default escaping
     },
