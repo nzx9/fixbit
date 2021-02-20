f0f64d0f3e2678df7bd3f5fe842a62b52a892af4
navindu <navindudananga123@gmail.com>
2021-02-20 09:56:30 +0530

    reducer upgraded

--- src/reducers/newCommentTracker.js
+++ src/reducers/newCommentTracker.js
@@ -4,13 +4,16 @@ const commentSlice = createSlice({
   name: "isNewComment",
   initialState: { value: false },
   reducers: {
-    toggleNewComment: (state) => {
-      state.value = !state;
+    newComment: (state) => {
+      state.value = true;
+    },
+    noNewComment: (state) => {
+      state.value = false;
     },
   },
 });
 
 export const newCommentStatus = (state) => state.isNewComment.value;
-export const { toggleNewComment } = commentSlice.actions;
+export const { newComment, noNewComment } = commentSlice.actions;
 
 export default commentSlice.reducer;
