526a236561d7399e76ebb097280d26910a4f017e
navindu <navindudananga123@gmail.com>
2021-02-21 10:13:40 +0530

    convertToLocalTime method updated

--- src/components/debugTools.js
+++ src/components/debugTools.js
@@ -4,11 +4,21 @@ export const DEBUG_PRINT = (msg) => {
   }
 };
 
-export const convertToLocalTime = (date_time, utc = true) => {
-  if (date_time !== null && date_time !== undefined) {
-    let date = new Date(date_time);
-    if (utc) date = new Date(date_time + " UTC");
-    return date.toString();
+export const convertToLocalTime = (dateTime, notStanderdFormat = true) => {
+  if (dateTime !== null && dateTime !== undefined) {
+    if (notStanderdFormat) {
+      // works for 2021-02-21 04:01:19
+      // convert to 2021-02-21T04:01:19.000000Z format before convert to local time
+      const splited = dateTime.split(" ");
+      let convertedDateTime = `${splited[0]}T${splited[1]}.000000Z`;
+      console.log(convertedDateTime);
+      const date = new Date(convertedDateTime);
+      return date.toString();
+    } else {
+      // works for 2021-02-20T17:52:45.000000Z or  1613639329186
+      const date = new Date(dateTime);
+      return date.toString();
+    }
   } else {
     return "Unknown";
   }
