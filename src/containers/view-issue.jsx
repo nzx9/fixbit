260dd72d68e16ff1cb06006d4a2f9ec58c37cc45
navindu <navindudananga123@gmail.com>
2021-03-02 13:04:18 +0530

    configurations in markdown. (html, xhtml tags support added)

--- src/containers/view-issue.jsx
+++ src/containers/view-issue.jsx
@@ -223,14 +223,14 @@ const ViewIssue = (props) => {
   const goto = useCallback((path) => history.push(path), [history]);
 
   var md = new Remarkable("full", {
-    html: false, // Enable HTML tags in source
-    xhtmlOut: false, // Use '/' to close single tags (<br />)
+    html: true, // Enable HTML tags in source
+    xhtmlOut: true, // Use '/' to close single tags (<br />)
     breaks: true, // Convert '\n' in paragraphs into <br>
     langPrefix: "language-", // CSS language prefix for fenced blocks
     linkTarget: "__blank", // set target to open link in
 
     // Enable some language-neutral replacements + quotes beautification
-    typographer: false,
+    typographer: true,
 
     // Double + single quotes replacement pairs, when typographer enabled,
     // and smartquotes on. Set doubles to '«»' for Russian, '„“' for German.
